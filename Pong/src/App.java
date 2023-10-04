e9ffe4628de7764fc6e27f7d7d3697c7f428c31b
Maximilian <maximilian.benedicto@live.se>
2023-10-04 16:50:21 +0200

    Added key detection

--- Pong/src/App.java
+++ Pong/src/App.java
@@ -1,30 +1,34 @@
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 
 public class App {
+
     public static void main(String[] args) throws Exception {
         Shapes shapes = new Shapes();
         JFrame frame = new JFrame();
-        frame.setSize(500,500);
+
+        frame.addKeyListener(new KeyAdapter() {
+            public void keyPressed(KeyEvent e) {
+                int keyCode = e.getKeyCode();
+                if (keyCode == KeyEvent.VK_UP) {
+                    shapes.moveBall(0, 1, 1);
+                    shapes.updateGraphics();
+                } else {
+                    shapes.moveBall(0, -1, -1);
+                    shapes.updateGraphics();
+                }
+            }
+        });
+
+        frame.setSize(500, 500);
         frame.setTitle("Pong");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
         frame.add(shapes);
-        frame.setResizable(false); 
-        shapes.newPaddle(10,10,10,10);
-        shapes.newPaddle(10,10,10,10);
-        int x = 0;
-        int y = 0;
-        for (int index = 0; index < 500; index++) {
-            shapes.setPosition(0, x, y);
-            shapes.setSize(0, x, y);
-            Thread.sleep(10);
-            x++;
-            y++;
-            shapes.updateGraphics();
-        }
-
-           
+        frame.setResizable(false);
+        shapes.newPaddle(10, 10, 10, 10);
+        shapes.newPaddle(10, 10, 10, 10);
+        shapes.newBall(250, 250, 10);
     }
 }
--- Pong/src/Shapes.java
+++ Pong/src/Shapes.java
@@ -1,7 +1,5 @@
 
 import javax.swing.JComponent;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
 import java.awt.*;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.Rectangle2D;
@@ -9,27 +7,32 @@ import java.util.ArrayList;
 
 public class Shapes extends JComponent{
     public ArrayList<Paddle> paddles = new ArrayList<>();
+    public ArrayList<Ball> balls = new ArrayList<>();
 
+    
+
+
+    public void movePaddle(int index, int x, int y){
+        Paddle specificShape = paddles.get(index);
+        specificShape.position[0] += x;
+        specificShape.position[1] += y;
+    }
     public void newPaddle(int x, int y, int w, int h){
         new Paddle(x,y,w,h);
     }
-
-    public void setPosition(int index, int x, int y){
+    public void setPositionPaddle(int index, int x, int y){
             Paddle specificShape = paddles.get(index);
             specificShape.position[0] = x;
             specificShape.position[1] = y;
     }  
-
-    public void setSize(int index, int x, int y){
+    public void setSizePaddle(int index, int w, int h){
             Paddle specificShape = paddles.get(index);
-            specificShape.size[0] = x;
-            specificShape.size[1] = y;
-    }  
-            
+            specificShape.size[0] = w;
+            specificShape.size[1] = h;
+    }        
     private class Paddle {
         int[] size = new int[2];
         int[] position = new int[2];
-        Color color = new Color(123);
         public Paddle(int x, int y, int w, int h){
             position[0] = x;
             position[1] = y;
@@ -39,9 +42,32 @@ public class Shapes extends JComponent{
         }
         
     }
-
-    public class Ball {
-
+    public void newBall(int x, int y, int r){
+        new Ball(x,y,r);
+    }
+    public void moveBall(int index, int x, int y){
+        Ball specificShape = balls.get(index);
+        specificShape.position[0] += x;
+        specificShape.position[1] += y;
+    }
+    public void setPositionBall(int index, int x, int y){
+            Ball specificShape = balls.get(index);
+            specificShape.position[0] = x;
+            specificShape.position[1] = y;
+    }  
+    public void setSizeBall(int index, int r){
+            Ball specificShape = balls.get(index);
+            specificShape.radius = r;
+    }
+    private class Ball {
+        int radius;
+        int[] position = new int[2];
+        public Ball(int x, int y, int r){
+            position[0] = x;
+            position[1] = y;
+            radius = r;
+            balls.add(this);
+        }
     }
 
     public void updateGraphics(){
@@ -54,6 +80,11 @@ public class Shapes extends JComponent{
             Rectangle2D.Double r = new Rectangle2D.Double(paddle.position[0], paddle.position[1], paddle.size[0], paddle.size[1]);
             g2d.setColor(Color.RED);
             g2d.fill(r);
+        }
+        for (Ball ball : balls) {
+            Ellipse2D.Double e = new Ellipse2D.Double(ball.position[0], ball.position[1], ball.radius, ball.radius);
+            g2d.setColor(Color.GREEN);
+            g2d.fill(e);
         }    
     }
 
