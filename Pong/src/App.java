850e422b6ee9879ff41f030ae1249f4b400300d5
Maximilian Benedicto <Maximilian>
2023-10-04 20:58:08 +0200

    Added basic physics. (no paddle collisions,.

--- Pong/src/App.java
+++ Pong/src/App.java
@@ -1,34 +1,71 @@
 import javax.swing.*;
+import java.awt.Color;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.util.HashSet;
+import java.util.Set;
 
 public class App {
+    private static Set<Integer> pressedKeys = new HashSet<>();
+    private static Shapes shapes; //creates a class field that can be modified outside of main method
 
     public static void main(String[] args) throws Exception {
-        Shapes shapes = new Shapes();
+        shapes = new Shapes();
         JFrame frame = new JFrame();
-
-        frame.addKeyListener(new KeyAdapter() {
-            public void keyPressed(KeyEvent e) {
-                int keyCode = e.getKeyCode();
-                if (keyCode == KeyEvent.VK_UP) {
-                    shapes.moveBall(0, 1, 1);
-                    shapes.updateGraphics();
-                } else {
-                    shapes.moveBall(0, -1, -1);
-                    shapes.updateGraphics();
-                }
-            }
-        });
-
-        frame.setSize(500, 500);
+        final int frameWidth = 500;
+        final int frameHeight = 500;
+        final double paddleIncrement = 0.5;
+        final double[] ballSpeed = {2.3,4.1};
+        frame.setSize(frameWidth, frameHeight);
         frame.setTitle("Pong");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
+        frame.setResizable(true);
         frame.add(shapes);
-        frame.setResizable(false);
-        shapes.newPaddle(10, 10, 10, 10);
-        shapes.newPaddle(10, 10, 10, 10);
-        shapes.newBall(250, 250, 10);
+        frame.getContentPane().setBackground(Color.BLACK);
+        shapes.frameWidth = frame.getContentPane().getWidth();
+        shapes.frameHeight = frame.getContentPane().getWidth();
+        shapes.paddleIncrement = paddleIncrement;
+        shapes.ballSpeed = ballSpeed;
+        shapes.newPaddle(0, 0, 10, 100);
+        shapes.newPaddle(shapes.frameWidth - 10, 0, 10, 100);
+        shapes.newBall((frameHeight / 2) - 5, (frameWidth / 2) - 5, 10);
+
+        frame.addKeyListener(new KeyAdapter() {
+            @Override //overrides method defined in KeyListener
+            public void keyPressed(KeyEvent e) {
+                int keyCode = e.getKeyCode();
+                pressedKeys.add(keyCode);
+                handleSimultaneousInput();
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                int keyCode = e.getKeyCode();
+                pressedKeys.remove(keyCode);
+                handleSimultaneousInput();
+                shapes.resetAccelerationPaddle();
+            }
+        });
+
+        while (true) {
+            Thread.sleep(10);
+            shapes.updateGraphics();
+        }
+    }
+
+    private static void handleSimultaneousInput() {
+        if (pressedKeys.contains(KeyEvent.VK_UP)) {
+            shapes.setAccelerationPaddle(1, -shapes.paddleIncrement);
+        }
+        if (pressedKeys.contains(KeyEvent.VK_DOWN)) {
+            shapes.setAccelerationPaddle(1, shapes.paddleIncrement);
+        }
+        if (pressedKeys.contains(KeyEvent.VK_W)) {
+            shapes.setAccelerationPaddle(0, -shapes.paddleIncrement);
+        }
+        if (pressedKeys.contains(KeyEvent.VK_S)) {
+            shapes.setAccelerationPaddle(0, shapes.paddleIncrement);
+        }
     }
 }
--- Pong/src/Shapes.java
+++ Pong/src/Shapes.java
@@ -9,13 +9,19 @@ public class Shapes extends JComponent{
     public ArrayList<Paddle> paddles = new ArrayList<>();
     public ArrayList<Ball> balls = new ArrayList<>();
 
+    double paddleIncrement;
+    double[] ballSpeed = new double[2];
+    int frameWidth;
+    int frameHeight;
     
-
-
-    public void movePaddle(int index, int x, int y){
+    public void setAccelerationPaddle(int index, double y){
         Paddle specificShape = paddles.get(index);
-        specificShape.position[0] += x;
-        specificShape.position[1] += y;
+        specificShape.acceleration = y;
+    }
+    public void resetAccelerationPaddle(){
+        for (Paddle paddle : paddles) {
+            paddle.acceleration = 0;
+        }
     }
     public void newPaddle(int x, int y, int w, int h){
         new Paddle(x,y,w,h);
@@ -32,7 +38,9 @@ public class Shapes extends JComponent{
     }        
     private class Paddle {
         int[] size = new int[2];
-        int[] position = new int[2];
+        double[] position = new double[2];
+        double speed = 0;
+        double acceleration = 0;
         public Paddle(int x, int y, int w, int h){
             position[0] = x;
             position[1] = y;
@@ -40,15 +48,9 @@ public class Shapes extends JComponent{
             size[1] = h;
             paddles.add(this);
         }
-        
     }
     public void newBall(int x, int y, int r){
         new Ball(x,y,r);
-    }
-    public void moveBall(int index, int x, int y){
-        Ball specificShape = balls.get(index);
-        specificShape.position[0] += x;
-        specificShape.position[1] += y;
     }
     public void setPositionBall(int index, int x, int y){
             Ball specificShape = balls.get(index);
@@ -61,7 +63,8 @@ public class Shapes extends JComponent{
     }
     private class Ball {
         int radius;
-        int[] position = new int[2];
+        double[] position = new double[2];
+        double[] speed = ballSpeed;
         public Ball(int x, int y, int r){
             position[0] = x;
             position[1] = y;
@@ -69,21 +72,35 @@ public class Shapes extends JComponent{
             balls.add(this);
         }
     }
-
     public void updateGraphics(){
+        for (Paddle paddle : paddles) {
+            paddle.speed += paddle.acceleration;
+            paddle.position[1] += paddle.speed;
+            paddle.position[1] = Math.max(0,Math.min(frameHeight-paddle.size[1],paddle.position[1]));
+        }
+        for (Ball ball : balls) {
+            ball.position[0] += ball.speed[0];
+            ball.position[1] += ball.speed[1];
+            if (ball.position[0] <= 0 || ball.position[0] >= frameWidth) {
+                ball.speed[0] = -ballSpeed[0];
+            } 
+            if (ball.position[1] <= 0 || ball.position[1] >= frameHeight) {
+                ball.speed[1] = -ballSpeed[1];
+            } 
+            
         repaint();
+        }
     }
     
     protected void paintComponent(Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
+        g2d.setColor(Color.WHITE);
         for (Paddle paddle : paddles) {
             Rectangle2D.Double r = new Rectangle2D.Double(paddle.position[0], paddle.position[1], paddle.size[0], paddle.size[1]);
-            g2d.setColor(Color.RED);
             g2d.fill(r);
         }
         for (Ball ball : balls) {
-            Ellipse2D.Double e = new Ellipse2D.Double(ball.position[0], ball.position[1], ball.radius, ball.radius);
-            g2d.setColor(Color.GREEN);
+            Ellipse2D.Double e = new Ellipse2D.Double(ball.position[0]-(ball.radius/2), ball.position[1]-(ball.radius/2), ball.radius, ball.radius);
             g2d.fill(e);
         }    
     }
